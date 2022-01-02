78c4cb658483f65b94e5ca262a0582d20aefed43
Venwiniar <53057766+Venwiniar@users.noreply.github.com>
2022-01-02 15:41:46 +0100

    rozpoczynamy booking

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
+	"github.com/venwiniar/myapp/pkg/config"
 	"github.com/venwiniar/myapp/pkg/handlers"
+	"github.com/venwiniar/myapp/pkg/render"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true // trwają pomimo zamknięcia przeglądarki przez klienta
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction // true do https prococol
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	_ = http.ListenAndServe(portNumber, nil)
+	// _ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
